8ef90482af51c79825bf6e65ebaa09831aa635d9
VrublevskiyVolodymyr <v637904@gmail.com>
2022-11-08 14:00:45 +0200

    Lesson3_main.js change break

--- Lesson3/main.js
+++ Lesson3/main.js
@@ -57,36 +57,41 @@ let a = +(prompt('Enter the week number'));
 switch (a) {
     case 1: {
         console.log('Monday');
-    }
         break;
+    }
+
     case 2: {
         console.log('Tuesday');
-    }
         break;
+    }
+
     case 3: {
         console.log('Wednesday');
-    }
         break;
+    }
+
     case 4: {
         console.log('Thursday');
-    }
         break;
+    }
+
     case 5: {
         console.log('Friday');
-    }
         break;
+    }
+
     case 6: {
         console.log('Saturday');
-    }
         break;
+    }
+
     case 7: {
         console.log('Sunday');
-    }
         break;
+    }
     default: {
         console.log('Wrong number');
     }
-        break;
 }
 
 //     - Користувач вводить або має два числа.

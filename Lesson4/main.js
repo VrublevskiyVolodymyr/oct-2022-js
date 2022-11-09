ed5ee82abea61f644e0bf1342bbd5c46af492577
VrublevskiyVolodymyr <v637904@gmail.com>
2022-11-09 23:39:00 +0200

    Add ${product.title} Lesson4_main.js

--- Lesson4/main.js
+++ Lesson4/main.js
@@ -83,7 +83,7 @@ let products = [
 for (let product of products) {
     document.write(`<div class="product-card">
     <h3 class="product-title">${product.title} - ${product.price} UAN</h3>
-<img src="${product.image}" alt="" class="product-image">
+<img src="${product.image}" alt="${product.title}" class="product-image">
 </div>`)
 }
 

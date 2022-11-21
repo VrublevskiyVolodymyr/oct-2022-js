36fcaaeab60400432600ad40818f12172c76a644
VrublevskiyVolodymyr <v637904@gmail.com>
2022-11-21 09:39:47 +0200

    remaking

--- Lesson5/Additional1/main.js
+++ Lesson5/Additional1/main.js
@@ -1,140 +1,130 @@
-// - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
-function area_rectangle(a, b) {
-    return a*b;
+// Всі функції повинні бути описані стрілочним типом!!!!
+//     - створити функцію яка приймає три числа та виводить найменьше. (Без Math.min!)
+let min = (a, b, c) => {
+    let m;
+    if (a < b && a < c)
+        m = a
+    else if (b < a && b < c)
+        m = b
+    else m = c;
+    console.log(m);
+    return m;
 }
-let area_ret=area_rectangle(10, 20)
-console.log('area_rectangle=', area_ret);
+min(7, 8, 11);
 
-// - створити функцію яка обчислює та повертає площу кола з радіусом r
-function area_circle(r) {
-    return Math.PI * r ** 2;
+// - створити функцію яка приймає три числа та виводить найбільше. (Без Math.max!)
+let max = (a, b, c) => {
+    let m;
+    if (a > b && a > c)
+        m = a
+    else if (b > a && b > c)
+        m = b
+    else m = c;
+    console.log(m);
+    return m;
 }
-let area_cir=area_circle(5)
-console.log('area_circle=', area_cir);
+max(8, 7, 6);
 
-// - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
-function area_cylinder(h, r) {
-    return 2 * Math.PI * r * (r + h);
-}
-
-let area_cyl = area_cylinder(5, 6);
-console.log('area_cylinder=', area_cyl);
-
-// - створити функцію яка приймає масив та виводить кожен його елемент
-function printerArray(array) {
-    for (const item of array) {
-        console.log(item);
-    }
-}
-
-let users = [
-    {name: 'vasya', age: 31, status: false},
-    {name: 'petya', age: 30, status: true},
-    {name: 'kolya', age: 29, status: true},
-    {name: 'olya', age: 28, status: false},
-    {name: 'max', age: 30, status: true},
-    {name: 'anya', age: 31, status: false},
-    {name: 'oleg', age: 28, status: false},
-    {name: 'andrey', age: 29, status: true},
-    {name: 'masha', age: 30, status: true},
-    {name: 'olya', age: 31, status: false},
-    {name: 'max', age: 31, status: true}
-];
-printerArray(users);
-
-// - створити функцію яка створює параграф з текстом. Текст задати через аргумент
-function printText(text) {
-    return (`<p>${text}</p>`);
-}
-
-let text = printText('Lorem ipsum dolor sit amet.')
-console.log(text);
-
-// - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
-function print_Ul_3li(text) {
-    return (`<ul>
-             <li>${text}</li>
-             <li>${text}</li>
-             <li>${text}</li>
-            </ul>`);
-}
-
-let Ul_3li_text = print_Ul_3li('Lorem ipsum dolor sit amet.')
-document.write(Ul_3li_text);
-
-// - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
-function print_Ul_nli(text, li) {
-    document.write('<ul>')
-    for (let i = 0; i < `${li}`; i++) {
-        document.write(`<li>${text}</li>`)
-    }
-    document.write('</ul>')
-}
-
-print_Ul_nli('Lorem ipsum dolor sit amet.', 3);
-
-// - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
-function printerArrayLi(array) {
-    document.write('<ul>')
-    for (const item of array) {
-        document.write(`<li>${item}</li>`);
-    }
-    document.write('</ul>')
-}
-
-let listOfItems = ['html', 21, true, 'mysql', 234, 'react', false, 'node.js'];
-printerArrayLi(listOfItems)
-
-// - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
-function printerArrayObject(array) {
-    document.write('<div class="container">')
-    for (const arrayElement of array) {
-        document.write('<div>')
-        for (const item in arrayElement) {
-            document.write(`${item}: ${arrayElement[item]}  `);
+// - створити функцію яка повертає найбільше число з масиву
+let arrMax = (arr) => {
+    let m = arr[0];
+    for (const item of arr) {
+        if (item > m) {
+            m = item;
         }
-        document.write('</div>')
     }
-    document.write('</div>')
+    return m
 }
-
-let users1 = [
-    {id: 0, name: 'vasya', age: 31},
-    {id: 1, name: 'petya', age: 30},
-    {id: 2, name: 'kolya', age: 29},
-    {id: 3, name: 'olya', age: 28},
-    {id: 4, name: 'max', age: 30},
-    {id: 5, name: 'anya', age: 31},
-    {id: 6, name: 'oleg', age: 28},
-    {id: 7, name: 'andrey', age: 29},
-    {id: 8, name: 'masha', age: 30},
-    {id: 9, name: 'olya', age: 31},
-    {id: 10, name: 'max', age: 31}
-];
-printerArrayObject(users1);
+let arr = [2, 4, 6, 8, 9, 12, -1, 67];
+console.log(arrMax(arr))
 
 // - створити функцію яка повертає найменьше число з масиву
-function minArray(array) {
-    let min = array[0];
-    for (const item of array) {
-        if (item<min)
-            min=item
-            }
-    return min
+let arrMin = (arr) => {
+    let m = arr[0];
+    for (const item of arr) {
+        if (item < m) {
+            m = item;
+        }
+    }
+    return m
 }
-let arr2 = [2, 4, 6, 8, 9, 12, -1, 67];
-let a = minArray(arr2);
-console.log(a);
+console.log(arrMin(arr));
 
 // - створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад [1,2,10]->13
-
-function summArray(array) {
+let arrSum = (arr) => {
     let sum = 0;
-    for (const item of array) {
+    for (const item of arr) {
         sum += item;
     }
     return sum;
 }
-arr2 = [2, 4, 6, 8, 9, 12, -1, 67];
-let s = summArray(arr2);
+let s = arrSum(arr);
 console.log(s);
+// - Дано натуральное число n. Выведите все числа от 1 до n.
+let appearN = (n) => {
+    let arr = [];
+    for (let i = 1; i <= n; i++) {
+        arr.push(i)
+    }
+    console.log(arr);
+    return arr
+}
+appearN(20)
+
+// - Даны два целых числа A и В . Выведите все числа от A до B включительно, в порядке возрастания, если A < B, или в порядке убывания в противном случае.
+let appearAB = (A, B) => {
+    let arr = [];
+    if (A < B) {
+        for (let i = A; i <= B; i++) {
+            arr.push(i)
+        }
+    } else {
+        for (let i = B; i <= A; i++) {
+            arr.push(i)
+        }
+    }
+    console.log(arr);
+    return arr
+}
+appearAB(10, 4)
+
+// -   функція Приймає масив та число "i", та міняє місцями об`єкт який знаходиться в індексі "i" на "i+1"
+//   EXAMPLE:
+//   foo([9,8,0,4], 0) // ==> [ 8, 9, 0, 4 ]
+//   foo([9,8,0,4], 1) // ==> [ 9 ,0, 8, 4 ]
+//   foo([9,8,0,4], 2) // ==> [ 9, 8, 4, 0 ]
+console.log(arr)
+let substitute = (arr, i) => {
+    for (let j = 0; j < arr.length; j++) {
+        if (j === i) {
+            let x = arr[j]
+            arr[j] = arr[j + 1];
+            arr[j + 1] = x;
+        }
+    }
+    console.log(arr);
+    return arr
+}
+arr = [2, 4, 6, 8, 9, 12, -1, 67];
+substitute(arr, 5);
+
+// - Сворити функцію яка буде переносити елементи з значенням 0 у кінець маисву. Зберігаючи при цьому порядок не нульових значень.
+// Двожина масиву від 2 до 100
+// EXAMPLE:
+// [1,0,6,0,3] => [1,6,3,0,0]
+// [0,1,2,3,4] => [1,2,3,4,0]
+// [0,0,1,0]   => [1,0,0,0]
+
+let transfer0 = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 0) {
+            arr.splice(i, 1);
+            arr[arr.length] = 0;
+        }
+    }
+    console.log(arr);
+    return arr
+}
+let arr1 = [2, 4, 0, 8, 0, 10, -1, 67]
+console.log(arr1)
+transfer0(arr1);
